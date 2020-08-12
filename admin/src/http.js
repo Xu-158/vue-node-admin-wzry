2579bdbd91d7dd55d1d0a2fba3796692c106f680
xujinfeng <1352176165@qq.com>
2020-08-12 19:45:34 +0800

    axios拦截器统一处理网络请求错误

--- admin/src/http.js
+++ admin/src/http.js
@@ -1,8 +1,21 @@
 import axios from 'axios'
+import Vue from 'vue'
 
 const http = axios.create({
-  baseURL:'http://localhost:3000/admin/api',
-  timeout:5000
+  baseURL: 'http://localhost:3000/admin/api',
+  timeout: 5000
+})
+
+http.interceptors.response.use(res => {
+  return res
+}, err => {
+  if (err.response.data.message) {
+    Vue.prototype.$message({
+      type: 'error',
+      message: err.response.data.message
+    })
+  }
+  return Promise.reject(err)
 })
 
 export default http
