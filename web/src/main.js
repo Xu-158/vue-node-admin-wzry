81b5232249538ba2abebb3ef7becfa76e68408ba
xujinfeng <1352176165@qq.com>
2020-08-14 20:46:18 +0800

    web端首页ing

--- web/src/main.js
+++ web/src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import './style.scss'
+import router from './router'
 
 Vue.config.productionTip = false
 
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
---
+++ web/src/views/Main.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div class="topbar bg-black py-2 px-3 d-flex ai-center">
+      <img src="../assets/logo.png" alt height="30" />
+      <div class="pl-2 flex-1">
+        <div class="text-white fs-md">王者荣耀</div>
+        <div class="text-grey-1 fs-xs">团队成就更多</div>
+      </div>
+      <button class="btn bg-primary">立即下载</button>
+    </div>
+    <div class="bg-primary py-3">
+      <div class="nav d-flex jc-around text-white">
+        <div class="nav-item active">
+          <router-link class="nav-link" to="/" tag="div">首页</router-link>
+        </div>
+        <div class="nav-item">
+          <router-link class="nav-link" to="/" tag="div">攻略中心</router-link>
+        </div>
+        <div class="nav-item">
+          <router-link class="nav-link" to="/" tag="div">赛事中心</router-link>
+        </div>
+      </div>
+    </div>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.nav {
+  .nav-item {
+    border-bottom: 3px solid transparent;
+    &.active {
+      border-bottom: 3px solid white;
+    }
+  }
+}
+</style>
