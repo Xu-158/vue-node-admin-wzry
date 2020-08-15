18ced4555c2e5c458f8f8b364e38aba0e0cfad82
xujinfeng <1352176165@qq.com>
2020-08-15 18:07:16 +0800

    精灵图应用

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,11 +1,26 @@
 <template>
-  <div v-swiper:mySwiper="swiperOption">
-    <div class="swiper-wrapper">
-      <div class="swiper-slide" :key="banner" v-for="banner in banners">
-        <img class="w-100" :src="banner" />
+  <div>
+    <div v-swiper:mySwiper="swiperOption">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide" :key="banner" v-for="banner in banners">
+          <img class="w-100" :src="banner" />
+        </div>
+      </div>
+      <div class="swiper-pagination pagination-home text-right px-3 py-1"></div>
+    </div>
+    <!--  -->
+    <div class="nav-icons mt-3 bg-white text-center pt-3 text-dark-1">
+      <div class="d-flex flex-wrap">
+        <div class="nav-item mb-3" v-for="item in 10" :key="item">
+          <i class="sprite sprite-news"></i>
+          <div class="py-2">爆料站</div>
+        </div>
+      </div>
+      <div class="bg-light py-2 fs-sm">
+        <i class="sprite sprite-arrow mr-1"></i>
+        <span>收起</span>
       </div>
     </div>
-    <div class="swiper-pagination pagination-home text-right px-3 py-1"></div>
   </div>
 </template>
 
@@ -41,6 +56,18 @@ export default {
     background-color: white;
     &.swiper-pagination-bullet-active {
       background-color: map-get($colors, "info");
+    }
+  }
+}
+
+.nav-icons {
+  border-top: 1px solid $border-color;
+  border-bottom: 1px solid $border-color;
+  .nav-item {
+    width: 25%;
+    border-right: 1px solid $border-color;
+    &:nth-child(4n) {
+      border-right: none;
     }
   }
 }
