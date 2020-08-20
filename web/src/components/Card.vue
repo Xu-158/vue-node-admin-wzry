89a20598d667e8a1d62b26811d6be61f2ba9a1e2
xujinfeng <1352176165@qq.com>
2020-08-20 16:06:34 +0800

    英雄详细

--- web/src/components/Card.vue
+++ web/src/components/Card.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <div class="card bg-white my-3 p-3">
+    <div class="card bg-white my-3 p-3" :class="{'pb-1':plain}">
       <div class="card-header d-flex ai-center">
-        <i class="iconfont fs-lg" :class="`icon-${iconClass}`"></i>
-        <div class="fs-xl flex-1 ml-1">{{title}}</div>
-        <i class="sprite sprite-ellipsis mt-1"></i>
+        <i v-if="!plain" class="iconfont fs-lg" :class="`icon-${iconClass}`"></i>
+        <strong class="fs-xl flex-1 ml-1">{{title}}</strong>
+        <i v-if="!plain" class="sprite sprite-ellipsis mt-1"></i>
       </div>
       <div class="px-4 w-100 pt-3" style="border-bottom:0.0385rem solid #d4d9de"></div>
       <div class="card-body">
@@ -25,6 +25,9 @@ export default {
     iconClass: {
       required: true,
       type: String,
+    },
+    plain:{
+      type:Boolean
     }
   }
 };
@@ -35,5 +38,8 @@ export default {
 .card {
   border-top:0.5px solid $border-color ;
   border-bottom:0.5px solid $border-color ;
+  .iconfont{
+    font-size: 1.2308rem;
+  }
 }
 </style>
--- web/src/views/Hero.vue
+++ web/src/views/Hero.vue
@@ -9,7 +9,10 @@
         <strong class="m-1">&gt;</strong>
       </router-link>
     </div>
-    <div class="header d-flex jc-end flex-column" :style="{'background-image':`url(${model.banner})`}">
+    <div
+      class="header d-flex jc-end flex-column"
+      :style="{'background-image':`url(${model.banner})`}"
+    >
       <div class="info text-white p-3">
         <div class="fs-sm">{{model.title}}</div>
         <h2 class="my-2">{{model.name}}</h2>
@@ -29,17 +32,106 @@
         </div>
       </div>
     </div>
+    <!--  -->
+    <div>
+      <div class="px-2 bg-white">
+        <div class="nav d-flex pb-1 pt-3 jc-around">
+          <div class="nav-item active">
+            <div class="nav-link">英雄初识</div>
+          </div>
+          <div class="nav-item">
+            <div class="nav-link">进阶攻略</div>
+          </div>
+        </div>
+      </div>
+      <swiper>
+        <swiper-slide>
+          <div>
+            <div class="d-flex bg-white py-3">
+              <router-link to="/" tag="button" class="btn btn-lg flex-1 ml-2">
+                <i class="iconfont icon-cc-menu-circle"></i>英雄介绍视频
+              </router-link>
+              <router-link to="/" tag="button" class="btn btn-lg flex-1 mx-2">
+                <i class="iconfont icon-video"></i>一图识英雄
+              </router-link>
+            </div>
+            <!-- skill -->
+            <div class="skills bg-white py-2">
+              <div class="d-flex jc-around px-3 py-2">
+                <img
+                  v-for="(item,index) in model.skills"
+                  :key="item._id"
+                  :src="item.icon"
+                  alt
+                  @click="currentSkillsIndex = index"
+                  :class="{skillActive:currentSkillsIndex === index}"
+                  class="skills skills-icon"
+                />
+              </div>
+              <div v-if="currentSkill">
+                <div class="d-flex px-3 ai-center mt-2">
+                  <strong class="pr-3 fs-lg">{{currentSkill.name}}</strong>
+                  <span>(冷却值：{{currentSkill.delay}}消耗：{{currentSkill.cost}})</span>
+                </div>
+                <div class="d-flex px-3 ai-center mt-1">
+                  <p>{{currentSkill.description}}</p>
+                </div>
+              </div>
+            </div>
+            <!-- skill-end -->
+            <card iconClass="cc-menu-circle hero-items" title="出装推荐">
+              <div class="fs-lg py-3">顺风出装</div>
+              <div class="d-flex jc-between text-center fs-sm">
+                <div v-for="item in model.items1" :key="item._id">
+                  <img class="item-icon" :src="item.icon" />
+                  <div>{{item.name}}</div>
+                </div>
+              </div>
+              <div style="border:0.5px solid #eee" class="mt-3"></div>
+              <div class="fs-lg py-3">逆风出装</div>
+              <div class="d-flex jc-between text-center fs-sm">
+                <div v-for="item in model.items2" :key="item._id">
+                  <img class="item-icon" :src="item.icon" />
+                  <div>{{item.name}}</div>
+                </div>
+              </div>
+            </card>
+            <card plain iconClass="cc-menu-circle" title="使用技巧">
+              <p>{{model.usageTips}}</p>
+            </card>
+            <card plain iconClass="cc-menu-circle" title="对抗技巧">
+              <p>{{model.battleTips}}</p>
+            </card>
+            <card plain iconClass="cc-menu-circle" title="团战思路">
+              <p>{{model.teamTips}}</p>
+            </card>
+            <card plain iconClass="cc-menu-circle" title="英雄关系">
+              <div class="fs-lg py-2">最佳搭档</div>
+              <div v-for="item in model.partners" :key="item._id" class="d-flex my-2">
+                <img class="mb-2" :src="item.hero.avatar" height="50px" alt />
+                <div class style="height:50px">{{item.description}}</div>
+              </div>
+              <div style="border:0.5px solid #eee" class="mt-3"></div>
+            </card>
+            <div></div>
+          </div>
+        </swiper-slide>
+        <swiper-slide></swiper-slide>
+      </swiper>
+    </div>
   </div>
 </template>
 
 <script>
+import Card from "@/components/Card";
 export default {
   props: {
     id: { type: String }
   },
   data() {
     return {
-      model: {}
+      model: {},
+      currentSkillsIndex: 0
     };
   },
   created() {
@@ -50,11 +142,20 @@ export default {
       const res = await this.$http.get(`/heroes/${this.id}`);
       this.model = res.data;
     }
+  },
+  computed: {
+    currentSkill() {
+      return this.model.skills[this.currentSkillsIndex];
+    }
+  },
+  components: {
+    Card
   }
 };
 </script>
 
 <style lang="scss">
+@import "../assets/scss/_variables.scss";
 .hero-page {
   .topbar {
     position: sticky;
@@ -64,12 +165,12 @@ export default {
   .header {
     height: 30vh;
     background: #fff no-repeat top center;
-    background-size: auto 100%;
+    background-size: 100% 100%;
   }
-  .info{
-    background: linear-gradient(rgba(0,0,0,0),rgba(0,0,0,1));
-    .scores{
-      .badge{
+  .info {
+    background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1));
+    .scores {
+      .badge {
         margin: 0 0.3rem;
         display: inline-block;
         border-radius: 50%;
@@ -81,6 +182,26 @@ export default {
         border: 0.5px solid white;
       }
     }
+  }
+
+  .nav {
+    border-bottom: 1px solid $border-color;
+  }
+  .skills {
+    .skills-icon {
+      width: 60px;
+      height: 60px;
+      border: 3px solid map-get($colors, "white");
+      border-radius: 50%;
+      &.skillActive {
+        border-color: map-get($colors, "primary");
+      }
+    }
+  }
+  .item-icon {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
   }
 }
 </style>
